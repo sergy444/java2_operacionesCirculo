727d5e7333b2a1f7b6569a6e7297f6b1e4472771
zubiri <zubiri@Ubuntu-12-04.(none)>
2014-09-23 09:34:33 +0200

    mas cositas

---
+++ src/OperacionesCirculo.java
@@ -0,0 +1,17 @@
+public class OperacionesCirculo {
+
+//Metodo para calcular el Área
+ public double area (double radio, double n){
+
+	double result = 0;
+	result = n * radio * radio;
+	return result;
+ }
+//Metodo para calcular la circunferencia
+ public double circun (double radio, double n){
+
+	double result = 0;
+	result = 2*radio*n;
+	return result;
+ }
+}
